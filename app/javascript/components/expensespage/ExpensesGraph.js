a8f8936ec089ceb4df1ccd9b42c39ddd6522b1fc
NIKHIL-KR-GARG <nikhil.kr.garg@gmail.com>
2024-11-28 18:36:08 +0800

    Made the graph to display dynamically on big and small screens

--- app/javascript/components/expensespage/ExpensesGraph.js
+++ app/javascript/components/expensespage/ExpensesGraph.js
@@ -10,26 +10,28 @@ const ExpensesGraph = ({expensesData}) => {
 
     const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#FF0000', '#800080', '#008000', '#000080'];
 
+    const isSmallScreen = window.innerHeight < 500;
+
     return (
         <Box display="flex" justifyContent="center" alignItems="center" p={1} width="100%">
-            <Grid container spacing={2} justifyContent="center" width="60%" border={1} borderColor="grey.400" bgcolor="#fff9e6" borderRadius={2}>
+            <Grid container spacing={2} justifyContent="center" width="60%" border={1} borderColor="grey.400" bgcolor="#fff9e6" borderRadius={2} style={{ height: '50vh' }}>
                 <Grid item size={12}>
-                    <Typography variant="h6" align="center" pb={1} pt={1}>
+                    <Typography variant="h6" align="center" pb={0} pt={1}>
                         Total Expenses: ({currentUserBaseCurrency}) {FormatCurrency(currentUserBaseCurrency, expensesData.reduce((total, item) => total + item.value, 0))}
                     </Typography>
                 </Grid>
-                <Grid item size={12} display="flex" justifyContent="center" style={{ height: '40vh' }}>
+                <Grid item size={12} display="flex" justifyContent="center" flexGrow={1}>
                     <ResponsiveContainer width="80%" height="100%">
-                    <PieChart width={400} height={400}>
-                            <Pie data={expensesData} cx="50%" cy="50%" outerRadius={150} fill="#8884d8" dataKey="value" label>
+                        <PieChart>
+                            <Pie data={expensesData} cx="50%" cy="50%" outerRadius="75%" fill="#8884d8" dataKey="value" label>
                                 {expensesData.map((entry, index) => (
                                     <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
                                 ))}
                             </Pie>
                             <Tooltip />
-                            <Legend />
+                            <Legend wrapperStyle={{ fontSize: isSmallScreen ? '10px' : '12px' }} />
                         </PieChart>
-                        </ResponsiveContainer>
+                    </ResponsiveContainer>
                 </Grid>
             </Grid>
         </Box>
