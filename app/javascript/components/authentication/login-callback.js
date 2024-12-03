292522f9bc88a6774e45173169b053884af8bfb5
NIKHIL-KR-GARG <nikhil.kr.garg@gmail.com>
2024-12-03 23:18:29 +0800

    changed http to https

--- app/javascript/components/authentication/login-callback.js
+++ app/javascript/components/authentication/login-callback.js
@@ -22,7 +22,7 @@ const LoginCallback = () => {
                     console.log('user:', user);
                      // Send a request to your Rails API to create or find the user
                     // const response = await axios.post('http://localhost:3000/api/users/create_or_find_user', {
-                        const response = await axios.post('http://www.finwise365.com/api/users/create_or_find_user', {
+                        const response = await axios.post('https://www.finwise365.com/api/users/create_or_find_user', {
                         email: user.email,
                     }, {
                         headers: {
