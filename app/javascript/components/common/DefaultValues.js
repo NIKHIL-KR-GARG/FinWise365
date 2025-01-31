d4c31baabb0bab7e356aefd32c564d2d9afd54b3
NIKHIL-KR-GARG <nikhil.kr.garg@gmail.com>
2025-01-31 09:29:13 +0800

    Updated Default values

--- app/javascript/components/common/DefaultValues.js
+++ app/javascript/components/common/DefaultValues.js
@@ -1,30 +1,82 @@
 export const HomeLoanRate = [
     { key: "SG", value: "2.4" },
-    { key: "IN", value: "8.5" }
+    { key: "IN", value: "3" },
+    { key: "US", value: "3" },
+    { key: "UK", value: "3" },
+    { key: "AU", value: "3" },
+    { key: "CN", value: "3" },
 ];
 
 export const HomeValueGrowthRate = [
-    { key: "SG", value: "3" },
-    { key: "IN", value: "5" }
+    { key: "SG", value: "5" },
+    { key: "IN", value: "8" },
+    { key: "US", value: "3" },
+    { key: "UK", value: "3" },
+    { key: "AU", value: "3" },
+    { key: "CN", value: "3" },
 ];
 
 export const VehicleLoanRate = [
     { key: "SG", value: "3.1" },
-    { key: "IN", value: "8.8" }
+    { key: "IN", value: "8.8" },
+    { key: "US", value: "5" },
+    { key: "UK", value: "5" },
+    { key: "AU", value: "5" },
+    { key: "CN", value: "5" },
 ];
 
 export const ExchangeRate = [
-    { from: "SGD", to: "INR", value: "60" },
-    { from: "INR", to: "SGD", value: "0.0158" },
-    { from: "USD", to: "SGD", value: "1.224" },
-    { from: "SGD", to: "USD", value: "0.7499" },
-    { from: "USD", to: "INR", value: "84.409" },
-    { from: "INR", to: "USD", value: "0.0118" }
+    { from: "SGD", to: "INR", value: "64" },
+    { from: "SGD", to: "USD", value: "0.739" },
+    { from: "SGD", to: "GBP", value: "0.595" },
+    { from: "SGD", to: "AUD", value: "1.189" },
+    { from: "SGD", to: "EUR", value: "0.711" },
+    { from: "SGD", to: "CNY", value: "5.356" },
+    { from: "INR", to: "SGD", value: "0.156" },
+    { from: "INR", to: "USD", value: "0.115" },
+    { from: "INR", to: "GBP", value: "0.009" },
+    { from: "INR", to: "AUD", value: "0.186" },
+    { from: "INR", to: "EUR", value: "0.111" },
+    { from: "INR", to: "CNY", value: "0.837" },
+    { from: "USD", to: "SGD", value: "1.354" },
+    { from: "USD", to: "INR", value: "84.643" },
+    { from: "USD", to: "GBP", value: "0.805" },
+    { from: "USD", to: "AUD", value: "1.61" },
+    { from: "USD", to: "EUR", value: "0.963" },
+    { from: "USD", to: "CNY", value: "7.251" },
+    { from: "GBP", to: "SGD", value: "1.681" },
+    { from: "GBP", to: "INR", value: "107.599" },
+    { from: "GBP", to: "USD", value: "1.242" },
+    { from: "GBP", to: "AUD", value: "1.999" },
+    { from: "GBP", to: "EUR", value: "1.195" },
+    { from: "GBP", to: "CNY", value: "9.004" },
+    { from: "AUD", to: "SGD", value: "0.841" },
+    { from: "AUD", to: "INR", value: "53.809" },
+    { from: "AUD", to: "USD", value: "0.621" },
+    { from: "AUD", to: "GBP", value: "0.5" },
+    { from: "AUD", to: "EUR", value: "0.598" },
+    { from: "AUD", to: "CNY", value: "4.503" },
+    { from: "EUR", to: "SGD", value: "1.406" },
+    { from: "EUR", to: "INR", value: "90.011" },
+    { from: "EUR", to: "USD", value: "1.039" },
+    { from: "EUR", to: "GBP", value: "0.837" },
+    { from: "EUR", to: "AUD", value: "1.673" },
+    { from: "EUR", to: "CNY", value: "7.533" },
+    { from: "CNY", to: "SGD", value: "0.187" },
+    { from: "CNY", to: "INR", value: "11.949" },
+    { from: "CNY", to: "USD", value: "0.138" },
+    { from: "CNY", to: "GBP", value: "0.111" },
+    { from: "CNY", to: "AUD", value: "0.222" },
+    { from: "CNY", to: "EUR", value: "0.133" }
 ];
 
 export const GrowthRate = [
     { key: "SG", value: "0" },
-    { key: "IN", value: "0" }
+    { key: "IN", value: "0" },
+    { key: "US", value: "0" },
+    { key: "UK", value: "0" },
+    { key: "AU", value: "0" },
+    { key: "CN", value: "0" },
 ];
 
 //export const CarLoanRate = [
